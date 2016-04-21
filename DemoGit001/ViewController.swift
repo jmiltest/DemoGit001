b68fcfa873bf0458801c282399f51c7e6d0c49a1
jmiltest <jankomilutinovic@Jankos-MacBook-Pro.local>
2016-04-21 19:22:01 +0200

    initial commit

--- DemoGit001/ViewController.swift
+++ DemoGit001/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var a = 1
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
